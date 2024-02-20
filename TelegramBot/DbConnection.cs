4da0d7934f36473563edfdc5c96c96b6364a759f
Иван Коноваленко <konovalenko-7575@outlook.com>
2024-02-20 10:33:21 +0300

    Add project files.

---
+++ TelegramBot/DbConnection.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using Telegram.Bot.Types;
+
+namespace TelegramBot
+{
+    class DbTgBot:IDisposable
+    {
+        private readonly SqlConnection connection;
+        public DbTgBot(string connectionString)
+        {
+            connection = new SqlConnection(connectionString);
+        }
+        public void CreateDb()
+        {
+            string sql = "IF DB_ID('tgbot') IS NOT NULL DROP DATABASE tgbot; " +
+                "CREATE DATABASE tgbot; " +
+                "USE tgbot; " +
+                "CREATE TABLE Queue([User] [nvarchar](max) NULL) " +
+                "CREATE TABLE ActiveChats([User1] [nvarchar](max) NULL,[User2] [nvarchar](max) NULL)";
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                command.CommandType = CommandType.Text;
+                command.ExecuteNonQuery();
+            }
+        }
+        public async Task InsertIntoActiveChats(string chatId, string chatId2 )
+        {
+            string sql = $"INSERT INTO ActiveChats (User1, User2) values('{chatId}', '{chatId2}')";
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                command.CommandType = CommandType.Text;
+                command.ExecuteNonQuery();
+            }
+        }
+        public async Task DeleteFromActiveChats(string chatId) 
+        {
+            string sql = $"Delete from ActiveChats where user1 ='{chatId}' or user2 ='{chatId}'";
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                command.CommandType = CommandType.Text;
+                command.ExecuteNonQuery();
+            }
+        }
+        public async Task<long> GetRecipientId(string chatId)
+        {
+            string sql = $"select * from ActiveChats where User1='{chatId}' or User2='{chatId}'";
+            SqlCommand myCommand = new SqlCommand(sql, connection);
+            using (SqlDataReader myDataReader = myCommand.ExecuteReader())
+            {
+                if (!myDataReader.Read()) return 0;
+                long recipientId = long.Parse(chatId);
+                if (myDataReader["User1"].ToString() == chatId)
+                {
+                    recipientId = long.Parse(myDataReader["User2"].ToString());
+                }
+                else if (myDataReader["User2"].ToString() == chatId)
+                {
+                    recipientId = long.Parse(myDataReader["User1"].ToString());
+                }
+                return recipientId;
+            }
+        }
+        public async Task InsertIntoQueue(string chatId)
+        {
+            string sql = $"INSERT INTO Queue([User]) values('{chatId}')";
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                command.CommandType = CommandType.Text;
+                command.ExecuteNonQuery();
+            }
+        }
+        public async Task DeleteFromQueue(string chatId)
+        {
+            string sql = $"Delete from Queue where [User]='{chatId}'";
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                command.CommandType = CommandType.Text;
+                command.ExecuteNonQuery();
+            }
+        }
+        public async Task<string> GetUserFromQueue()
+        {
+            string sql = $"select * from Queue";
+            using (SqlCommand myCommand = new SqlCommand(sql, connection))
+            {
+                using (SqlDataReader myDataReader = myCommand.ExecuteReader())
+                {
+                    if (myDataReader.Read()) return myDataReader["User"].ToString();
+                    else return "0";
+                }
+            }
+        }
+
+        public void Open()
+        {
+            connection.Open();
+        }
+        public void Close()
+        {
+            Dispose();
+        }
+        public void Dispose()
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ TelegramBot/Program.cs
@@ -0,0 +1,153 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using TelegramBot;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true);
+var root=builder.Build();
+
+var botClient = new TelegramBotClient(root["token"].ToString());
+
+using var dbConnection = new DbTgBot(root["stringConnection"].ToString());
+dbConnection.Open();
+dbConnection.CreateDb();
+
+using CancellationTokenSource cts = new();
+
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>() 
+};
+
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+cts.Cancel();
+
+
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    if (update.Message is not { } message)
+        return;
+    // Only process text messages
+    if (message.Text is not { } messageText)
+        return;
+
+    var chatId = message.Chat.Id;
+
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+    if (message.Text == "/find")
+    {
+        long recipientId = await dbConnection.GetRecipientId(chatId.ToString());
+        if (recipientId != 0)
+        {
+            Message sentMessage = await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "Вы уже находитесь в чате\n/stop-закончить диалог",
+            cancellationToken: cancellationToken);
+            return;
+        }
+        string userFromQueue = await dbConnection.GetUserFromQueue();
+        if (userFromQueue == chatId.ToString()) 
+        {
+            Message sentMessage = await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "Вы уже добавлены в очередь, ожидайте собеседника",
+            cancellationToken: cancellationToken);
+            return; 
+        }
+        if (userFromQueue == "0") 
+        {
+            await dbConnection.InsertIntoQueue(chatId.ToString());
+            Message sentMessage = await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "Вы добавлены в очередь, ожидайте собеседника",
+            cancellationToken: cancellationToken);
+        }
+        else
+        {
+            await dbConnection.InsertIntoActiveChats(chatId.ToString(), userFromQueue);
+            await dbConnection.DeleteFromQueue(userFromQueue);
+            Message sentMessage = await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "Собеседник найден",
+            cancellationToken: cancellationToken);
+            Message sentMessage1 = await botClient.SendTextMessageAsync(
+            chatId: userFromQueue,
+            text: "Собеседник найден",
+            cancellationToken: cancellationToken);
+        }
+
+    }
+    else if (message.Text == "/stop")
+    {
+        long recipientId=await dbConnection.GetRecipientId(chatId.ToString());
+        if (recipientId == 0) 
+        {
+            await dbConnection.DeleteFromQueue(chatId.ToString());
+            Message sentMessage = await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "Вы удалены из очереди",
+            cancellationToken: cancellationToken);
+            return; 
+        }
+        else
+        {
+            await dbConnection.DeleteFromActiveChats(chatId.ToString());
+            Message sentMessage = await botClient.SendTextMessageAsync(
+            chatId: recipientId,
+            text: "Собеседник закончил чат",
+            cancellationToken: cancellationToken);
+            Message sentMessage1 = await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "Вы закончили чат",
+            cancellationToken: cancellationToken);
+        }
+    }
+    else
+    {
+        long recipientId = await dbConnection.GetRecipientId(chatId.ToString());
+        if (recipientId == 0)
+        {
+            Message sentMessage = await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "Вы не находитесь в чате\n/find-Найти собеседника",
+            cancellationToken: cancellationToken);
+            return;
+        }
+        else
+        {
+            Message sentMessage = await botClient.SendTextMessageAsync(
+             chatId: recipientId,
+             text: messageText,
+             cancellationToken: cancellationToken);
+        }
+    }
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
